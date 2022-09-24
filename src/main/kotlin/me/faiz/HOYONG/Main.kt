73202fa224bf867dd8aa7568c8df2e630884b018
faiz36 <lovekh3549@gmail.com>
2022-09-25 00:04:22 +0900

    ✨Removed All ban Item & Added Totem of Undying cooltime(Prototype)

--- src/main/kotlin/me/faiz/HOYONG/Main.kt
+++ src/main/kotlin/me/faiz/HOYONG/Main.kt
@@ -6,13 +6,16 @@ import net.kyori.adventure.sound.Sound
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.event.ClickEvent
 import net.kyori.adventure.text.event.HoverEvent
+import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.enchantments.Enchantment
 import org.bukkit.entity.EntityType
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.enchantment.EnchantItemEvent
 import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.entity.EntityResurrectEvent
 import org.bukkit.event.inventory.CraftItemEvent
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.inventory.InventoryType
@@ -23,16 +26,31 @@ import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
+import org.bukkit.scheduler.BukkitRunnable
 
 
 @Suppress("unused")
 class Main:JavaPlugin(),Listener {
 
     var atk = true
+    var cooltime:HashMap<Player,Long> = HashMap()
 
     override fun onEnable() {
         server.pluginManager.registerEvents(this,this)
+        val yt = object : BukkitRunnable() {
+            override fun run() {
+                val pls = server.onlinePlayers
+                pls.forEach {
+                    if(it.hasPermission("hoyong.yt")){
+                        it.addPotionEffect(PotionEffect(PotionEffectType.REGENERATION,1,1))
+                        it.addPotionEffect(PotionEffect(PotionEffectType.SPEED,1,1))
+                    }
+                }
+            }
 
+        }.runTaskTimer(this@Main,10L,10L)
         kommand {
             register("디스코드"){
                 executes {
@@ -47,17 +65,27 @@ class Main:JavaPlugin(),Listener {
                 }
             }
             register("smp"){
-                requires { player.isOp }
+                requires { sender.isOp }
                 then("start"){
                     executes {
                         server.getWorld("world")!!.worldBorder.size = 10000.0
                         atk = false
-                        server.scheduler.runTaskLater(this@Main,{->
-                            Runnable {
-                                atk=true
-                                server.broadcast(Component.text("10분이 지나 PVP가 가능합니다!"))
+                        var time = 600
+                        val runnable = object : BukkitRunnable() {
+                            override fun run() {
+                                if(time > 0){
+                                    val pls = server.onlinePlayers
+                                    time--
+                                    pls.forEach {
+                                        it.sendActionBar(Component.text("PVP 가능 까지 ${time}초 남았습니다"))
+                                    }
+                                }else if(time==0){
+                                    atk=true
+                                    server.broadcast(Component.text("§l이제 PVP가 가능합니다!"))
+                                    cancel()
+                                }
                             }
-                        },20L*60L*10L)
+                        }.runTaskTimer(this@Main,20L*1L,20L*1L)
                     }
                 }
             }
@@ -65,12 +93,54 @@ class Main:JavaPlugin(),Listener {
     }
 
     @EventHandler
-    fun onJoin(e:EntityDamageByEntityEvent){
-        if(e.damager.type != EntityType.PLAYER){
-            return
+    fun onTotem(e: EntityResurrectEvent){
+        var pl: Player? = server.getPlayer(e.entity.name) ?: return
+        if(e.isCancelled) return
+        if(cooltime[pl] == null || cooltime[pl]!! + 600 >= (System.currentTimeMillis() / 1000)){
+            cooltime[pl!!] = System.currentTimeMillis()/1000
+        }else{
+            e.isCancelled=true
         }
-        if(e.entity.type==EntityType.PLAYER && !atk){
+    }
+
+    @EventHandler
+    fun onPreprocess(e: PlayerCommandPreprocessEvent){
+        if(e.message == "pl"){
             e.isCancelled = true
+            e.player.sendMessage("§c이 명령어는 사용할 수 없습니다")
+        }
+        if(e.message == "plugins"){
+            e.isCancelled = true
+            e.player.sendMessage("§c이 명령어는 사용할 수 없습니다")
+        }
+        if(e.message == "stop"){
+            e.isCancelled = true
+            e.player.sendMessage("§c이 명령어는 사용할 수 없습니다")
+        }
+        if(e.message == "ver"){
+            e.isCancelled = true
+            e.player.sendMessage("§c이 명령어는 사용할 수 없습니다")
+        }
+        if(e.message == "version"){
+            e.isCancelled = true
+            e.player.sendMessage("§c이 명령어는 사용할 수 없습니다")
+        }
+        if(e.message == "help"){
+            e.isCancelled = true
+            e.player.sendMessage("§c이 명령어는 사용할 수 없습니다")
+        }
+        if(e.message == "?"){
+            e.isCancelled = true
+            e.player.sendMessage("§c이 명령어는 사용할 수 없습니다")
+        }
+
+        if(e.message == "op"){
+            e.isCancelled = true
+            e.player.sendMessage("§c이 명령어는 사용할 수 없습니다")
+        }
+        if(e.message == "deop"){
+            e.isCancelled = true
+            e.player.sendMessage("§c이 명령어는 사용할 수 없습니다")
         }
     }
 
@@ -84,150 +154,109 @@ class Main:JavaPlugin(),Listener {
         e.quitMessage(Component.text("§e${e.player.name}이(가) SMP에 떠났습니다"))
     }
 
-    @EventHandler
-    fun onPickup(e:PlayerAttemptPickupItemEvent){
-        val pl = e.player
-        if(pl.hasPermission("hoyong.yt")||pl.isOp) return
-        val ci = e.item.itemStack
-        if(ci == ItemStack(Material.TOTEM_OF_UNDYING)||
-            ci == ItemStack(Material.DIAMOND_SWORD)||
-            ci == ItemStack(Material.NETHERITE_SWORD)||
-            ci == ItemStack(Material.NETHERITE_AXE)||
-            ci == ItemStack(Material.NETHERITE_HELMET)||
-            ci == ItemStack(Material.NETHERITE_CHESTPLATE)||
-            ci == ItemStack(Material.NETHERITE_LEGGINGS)||
-            ci == ItemStack(Material.NETHERITE_BOOTS)) {
-            e.isCancelled = true
-        }
-    }
+//    @EventHandler
+//    fun onPickup(e:PlayerAttemptPickupItemEvent){
+//        val pl = e.player
+//        if(pl.hasPermission("hoyong.yt")||pl.isOp) return
+//        val ci = e.item.itemStack
+//        if(ci.type == Material.TOTEM_OF_UNDYING||
+//            ci.type == Material.DIAMOND_SWORD||
+//            ci.type == Material.NETHERITE_SWORD||
+//            ci.type == Material.NETHERITE_AXE||
+//            ci.type == Material.NETHERITE_HELMET||
+//            ci.type == Material.NETHERITE_CHESTPLATE||
+//            ci.type == Material.NETHERITE_LEGGINGS||
+//            ci.type == Material.NETHERITE_BOOTS) {
+//            e.isCancelled = true
+//        }
+//    }
 
-    @EventHandler
-    fun onInv(e:InventoryClickEvent){
-        val inv = e.clickedInventory?.type
-        if(inv == InventoryType.PLAYER||
-            inv == InventoryType.SMITHING||
-            inv == InventoryType.CRAFTING) return
-        val pl = e.whoClicked
-        if(pl.hasPermission("hoyong.yt")||pl.isOp) return
-        val ci = e.currentItem
-        if(inv == InventoryType.ANVIL){
-            if(ci != null){
-                if(ci == ItemStack(Material.DIAMOND_AXE)||
-                        ci.enchantments.containsKey(Enchantment.DAMAGE_ALL)){
-                    e.isCancelled = true
-                    pl.sendMessage("§c이 인챈트를 하실 수 없습니다")
-                    pl.playSound(Sound.sound(Key.key("block.anvil.place"),Sound.Source.BLOCK,1f,1f))
-                }
-            if(ci == ItemStack(Material.BOW) &&
-                ci.enchantments.containsKey(Enchantment.ARROW_DAMAGE) ||
-                ci.enchantments.containsValue(4)||
-                ci.enchantments.containsValue(5)) {
-                e.isCancelled = true
-                pl.sendMessage("§c이 인챈트를 하실 수 없습니다")
-                pl.playSound(Sound.sound(Key.key("block.anvil.place"),Sound.Source.BLOCK,1f,1f))
-            }
-            }
-        }
-        if(ci == ItemStack(Material.TOTEM_OF_UNDYING)||
-            ci == ItemStack(Material.DIAMOND_SWORD)||
-            ci == ItemStack(Material.NETHERITE_SWORD)||
-            ci == ItemStack(Material.NETHERITE_AXE)||
-            ci == ItemStack(Material.NETHERITE_HELMET)||
-            ci == ItemStack(Material.NETHERITE_CHESTPLATE)||
-            ci == ItemStack(Material.NETHERITE_LEGGINGS)||
-            ci == ItemStack(Material.NETHERITE_BOOTS)){
-            e.isCancelled = true
-            pl.sendMessage("§c이 아이템은 이동하실 수 없습니다")
-            pl.playSound(Sound.sound(Key.key("block.anvil.place"),Sound.Source.BLOCK,1f,1f))
-        }
-    }
+//    @EventHandler
+//    fun onInv(e:InventoryClickEvent){
+//        val inv = e.clickedInventory?.type
+//        if(inv == InventoryType.PLAYER||
+//            inv == InventoryType.SMITHING||
+//            inv == InventoryType.CRAFTING) return
+//        val pl = e.whoClicked
+//        if(pl.hasPermission("hoyong.yt")||pl.isOp) return
+//        val ci = e.currentItem
+//        if(inv == InventoryType.ANVIL){
+//            if(ci != null){
+//                if(ci.type == Material.DIAMOND_SWORD &&
+//                        ci.enchantments.containsKey(Enchantment.DAMAGE_ALL)){
+//                    e.isCancelled = true
+//                    pl.sendMessage("§c이 인챈트를 하실 수 없습니다")
+//                    pl.playSound(Sound.sound(Key.key("block.anvil.place"),Sound.Source.BLOCK,1f,1f))
+//                }
+//            if(ci.type == Material.BOW &&
+//                ci.enchantments.containsKey(Enchantment.ARROW_DAMAGE) ||
+//                ci.enchantments.containsValue(4)||
+//                ci.enchantments.containsValue(5)) {
+//                e.isCancelled = true
+//                pl.sendMessage("§c이 인챈트를 하실 수 없습니다")
+//                pl.playSound(Sound.sound(Key.key("block.anvil.place"),Sound.Source.BLOCK,1f,1f))
+//            }
+//            }
+//        }
+//        if(ci?.type == Material.TOTEM_OF_UNDYING||
+//            ci?.type == Material.DIAMOND_SWORD||
+//            ci?.type == Material.NETHERITE_SWORD||
+//            ci?.type == Material.NETHERITE_AXE||
+//            ci?.type == Material.NETHERITE_HELMET||
+//            ci?.type == Material.NETHERITE_CHESTPLATE||
+//            ci?.type == Material.NETHERITE_LEGGINGS||
+//            ci?.type == Material.NETHERITE_BOOTS){
+//            e.isCancelled = true
+//            pl.sendMessage("§c이 아이템은 이동하실 수 없습니다")
+//            pl.playSound(Sound.sound(Key.key("block.anvil.place"),Sound.Source.BLOCK,1f,1f))
+//        }
+//    }
+//
+//    @EventHandler
+//    fun onEnchant(e:EnchantItemEvent){
+//        val pl = e.enchanter
+//        if(pl.hasPermission("hoyong.yt")||pl.isOp) return
+//        if(e.enchantsToAdd.containsKey(Enchantment.ARROW_DAMAGE) &&
+//            e.enchantsToAdd.containsValue(4)||
+//            e.enchantsToAdd.containsValue(5)){
+//            e.isCancelled = true
+//            pl.sendMessage("§c이 인첸트를 하실 수 없습니다(힘4&5 인첸트가 아닌데 뜬다면 추가 인첸트로 떠서 그렇습니다)")
+//            pl.playSound(Sound.sound(Key.key("block.anvil.place"),Sound.Source.BLOCK,1f,1f))
+//        }
+//        if(e.item.type == Material.DIAMOND_AXE && e.enchantsToAdd.containsKey(Enchantment.DAMAGE_ALL)){
+//            e.isCancelled = true
+//            pl.sendMessage("§c이 인첸트를 하실 수 없습니다(날카로움 인첸트가 아닌데 뜬다면 추가 인첸트로 떠서 그렇습니다)")
+//            pl.playSound(Sound.sound(Key.key("block.anvil.place"),Sound.Source.BLOCK,1f,1f))
+//        }
+//    }
+//
+//    @EventHandler
+//    fun onSmith(e:SmithItemEvent){
+//        val pl = e.whoClicked
+//        val ci = e.currentItem
+//        if(pl.hasPermission("hoyong.yt")||pl.isOp) return
+//        if(ci?.type == Material.NETHERITE_SWORD||
+//            ci?.type == Material.NETHERITE_AXE||
+//            ci?.type == Material.NETHERITE_HELMET||
+//            ci?.type == Material.NETHERITE_CHESTPLATE||
+//            ci?.type == Material.NETHERITE_LEGGINGS||
+//            ci?.type == Material.NETHERITE_BOOTS){
+//            e.isCancelled = true
+//            pl.sendMessage("§c이 아이템은 제작하실 수 없습니다")
+//            pl.playSound(Sound.sound(Key.key("block.anvil.place"),Sound.Source.BLOCK,1f,1f))
+//        }
+//    }
+//
+//    @EventHandler
+//    fun onCraft(e:CraftItemEvent){
+//        val pl = e.whoClicked
+//        val ci = e.currentItem
+//        if(pl.hasPermission("hoyong.yt")||pl.isOp) return
+//        if(ci?.type == Material.DIAMOND_SWORD){
+//            e.isCancelled = true
+//            pl.sendMessage("§c이 아이템은 제작하실 수 없습니다")
+//            pl.playSound(Sound.sound(Key.key("block.anvil.place"),Sound.Source.BLOCK,1f,1f))
+//        }
+//    }
 
-    @EventHandler
-    fun onEnchant(e:EnchantItemEvent){
-        val pl = e.enchanter
-        if(pl.hasPermission("hoyong.yt")||pl.isOp) return
-        if(e.enchantsToAdd.containsKey(Enchantment.ARROW_DAMAGE) &&
-            e.enchantsToAdd.containsValue(4)||
-            e.enchantsToAdd.containsValue(5)){
-            e.isCancelled = true
-            pl.sendMessage("§c이 인첸트를 하실 수 없습니다(힘4&5 인첸트가 아닌데 뜬다면 추가 인첸트로 떠서 그렇습니다)")
-            pl.playSound(Sound.sound(Key.key("block.anvil.place"),Sound.Source.BLOCK,1f,1f))
-        }
-        if(e.item == ItemStack(Material.DIAMOND_AXE)&&e.enchantsToAdd.containsKey(Enchantment.DAMAGE_ALL)){
-            e.isCancelled = true
-            pl.sendMessage("§c이 인첸트를 하실 수 없습니다(날카로움 인첸트가 아닌데 뜬다면 추가 인첸트로 떠서 그렇습니다)")
-            pl.playSound(Sound.sound(Key.key("block.anvil.place"),Sound.Source.BLOCK,1f,1f))
-        }
-    }
-
-    @EventHandler
-    fun onSmith(e:SmithItemEvent){
-        val pl = e.whoClicked
-        val ci = e.currentItem
-        if(pl.hasPermission("hoyong.yt")||pl.isOp) return
-        if(ci == ItemStack(Material.NETHERITE_SWORD)||
-            ci == ItemStack(Material.NETHERITE_AXE)||
-            ci == ItemStack(Material.NETHERITE_HELMET)||
-            ci == ItemStack(Material.NETHERITE_CHESTPLATE)||
-            ci == ItemStack(Material.NETHERITE_LEGGINGS)||
-            ci == ItemStack(Material.NETHERITE_BOOTS)){
-            e.isCancelled = true
-            pl.sendMessage("§c이 아이템은 제작하실 수 없습니다")
-            pl.playSound(Sound.sound(Key.key("block.anvil.place"),Sound.Source.BLOCK,1f,1f))
-        }
-    }
-
-    @EventHandler
-    fun onCraft(e:CraftItemEvent){
-        val pl = e.whoClicked
-        val ci = e.currentItem
-        if(pl.hasPermission("hoyong.yt")||pl.isOp) return
-        if(ci == ItemStack(Material.DIAMOND_SWORD)){
-            e.isCancelled = true
-            pl.sendMessage("§c이 아이템은 제작하실 수 없습니다")
-            pl.playSound(Sound.sound(Key.key("block.anvil.place"),Sound.Source.BLOCK,1f,1f))
-        }
-    }
-
-
-    @EventHandler
-    fun onPreprocess(e: PlayerCommandPreprocessEvent){
-        if(e.message == "pl"){
-            e.isCancelled = true
-            e.player.sendMessage("§c이 명령어는 사용할 수 없습니다")
-        }
-        else if(e.message == "plugins"){
-            e.isCancelled = true
-            e.player.sendMessage("§c이 명령어는 사용할 수 없습니다")
-        }
-        else if(e.message == "stop"){
-            e.isCancelled = true
-            e.player.sendMessage("§c이 명령어는 사용할 수 없습니다")
-        }
-        else if(e.message == "ver"){
-            e.isCancelled = true
-            e.player.sendMessage("§c이 명령어는 사용할 수 없습니다")
-        }
-        else if(e.message == "version"){
-            e.isCancelled = true
-            e.player.sendMessage("§c이 명령어는 사용할 수 없습니다")
-        }
-        else if(e.message == "help"){
-            e.isCancelled = true
-            e.player.sendMessage("§c이 명령어는 사용할 수 없습니다")
-        }
-        else if(e.message == "?"){
-            e.isCancelled = true
-            e.player.sendMessage("§c이 명령어는 사용할 수 없습니다")
-        }
-
-        else if(e.message == "op"){
-            e.isCancelled = true
-            e.player.sendMessage("§c이 명령어는 사용할 수 없습니다")
-        }
-        else if(e.message == "deop"){
-            e.isCancelled = true
-            e.player.sendMessage("§c이 명령어는 사용할 수 없습니다")
-        }
-    }
-} //for test
+}
